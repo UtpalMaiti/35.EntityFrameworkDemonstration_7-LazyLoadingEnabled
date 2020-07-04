70b11253ecd8fe6ff3acec474114f02559f8ef5f
Utpal Maiti <Utpalmaiti12@gmail.com>
2020-07-05 02:00:37 +0530

    Add project files.

---
+++ EntityFrameworkDemonstration_7/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace EntityFrameworkDemonstration_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmployeeDBEntities dbContext = new EmployeeDBEntities();
+
+            //dbContext.Configuration.LazyLoadingEnabled = false;
+
+            var deptData = dbContext.DeptInfoes.Include(d => d.EmpInfoes).Include(d=>d.EmpInfoes);
+
+            foreach(var dept in deptData)
+            {
+                Console.WriteLine(dept.Id+"\t"+dept.Name);
+
+                Console.WriteLine("-------------------------------------------");
+
+                foreach(var emp in dept.EmpInfoes)
+                {
+                    Console.WriteLine(emp.Id+"\t"+emp.Name+"\t"+emp.Salary);
+                }
+
+                Console.WriteLine("\n");
+            }
+        }
+    }
+}
